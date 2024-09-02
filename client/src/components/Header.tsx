56b71942176fd351965b08fcb6cff8d7729f8880
TeresiaWanjiru <terrymunderu@gmail.com>
2024-09-02 16:30:56 +0300

    Initial commit

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { FaCartArrowDown } from "react-icons/fa"
+
+
+const Header = () => {
+  return (
+    <>
+    <h1>Udemy</h1>
+    <h4>Categories</h4>
+    <input placeholder="Search for anything" />
+    <h4>Categories</h4>
+    <h4>Categories</h4>
+    <FaCartArrowDown />
+
+    </>
+  )
+}
+
+export default Header
